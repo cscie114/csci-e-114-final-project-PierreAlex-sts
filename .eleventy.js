ce50295206f051c613ddcda58a4d29adc938dd78
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-05-09 15:19:56 -0400

    nothing

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/**");
     eleventyConfig.addPassthroughCopy("src/_data");
     eleventyConfig.addPassthroughCopy("src/");
+    eleventyConfig.addPlugin(EleventyEdgePlugin);
     
     return {
         dir: {
---
+++ netlify/edge-functions/eleventy-edge.js
@@ -0,0 +1,31 @@
+import {
+  EleventyEdge,
+  precompiledAppData,
+} from "./_generated/eleventy-edge-app.js";
+
+export default async (request, context) => {
+  try {
+    let edge = new EleventyEdge("edge", {
+      request,
+      context,
+      precompiled: precompiledAppData,
+
+      // default is [], add more keys to opt-in e.g. ["appearance", "username"]
+      cookies: [],
+    });
+
+    edge.config((eleventyConfig) => {
+      // Add some custom Edge-specific configuration
+      // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+      eleventyConfig.addGlobalData("date", context.date);
+      eleventyConfig.addFilter("nextRace", (races) => {
+        console.log(races);
+      })
+      return});
+
+    return await edge.handleResponse();
+  } catch (e) {
+    console.log("ERROR", { e });
+    return context.next(e);
+  }
+};
