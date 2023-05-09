40a4defb1cc9860cf6bc3298459bfc24981ec96d
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-05-09 14:34:01 -0400

    failed at edge functions

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { EleventyEdgePlugin } = require("@11ty/eleventy");
+
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/**");
     eleventyConfig.addPassthroughCopy("src/_data");
     eleventyConfig.addPassthroughCopy("src/");
+    eleventyConfig.addPlugin(EleventyEdgePlugin);
     
     return {
         dir: {
