f6b69ca876feeec60932b95c0c75a8c843046336
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-05-09 15:00:34 -0400

    removing edge totally

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/**");
     eleventyConfig.addPassthroughCopy("src/_data");
     eleventyConfig.addPassthroughCopy("src/");
-    eleventyConfig.addPlugin(EleventyEdgePlugin);
     
     return {
         dir: {
