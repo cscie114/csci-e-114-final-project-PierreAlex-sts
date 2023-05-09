1f28a4470da9d4f894b75f8ed15ede197e8c405f
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-05-09 14:57:28 -0400

    this is crazy

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,35 +1,35 @@
-import {
-  EleventyEdge,
-  precompiledAppData,
-} from "./_generated/eleventy-edge-app.js";
+// import {
+//   EleventyEdge,
+//   precompiledAppData,
+// } from "./_generated/eleventy-edge-app.js";
 
-export default async (request, context) => {
-  try {
-    let edge = new EleventyEdge("edge", {
-      request,
-      context,
-      precompiled: precompiledAppData,
+// export default async (request, context) => {
+//   try {
+//     let edge = new EleventyEdge("edge", {
+//       request,
+//       context,
+//       precompiled: precompiledAppData,
 
-      // default is [], add more keys to opt-in e.g. ["appearance", "username"]
-      cookies: [],
-    });
-    edge.config((eleventyConfig) => {
-      // Add some custom Edge-specific configuration
-      // e.g. Fancier json output
-      eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
-      eleventyConfig.addGlobalData("date", context.date);
-      eleventyConfig.addFilter("nextRace", (races) => {
-        console.log(races);
-        return;
-      })
-    });
+//       // default is [], add more keys to opt-in e.g. ["appearance", "username"]
+//       cookies: [],
+//     });
+//     edge.config((eleventyConfig) => {
+//       // Add some custom Edge-specific configuration
+//       // e.g. Fancier json output
+//       eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+//       eleventyConfig.addGlobalData("date", context.date);
+//       eleventyConfig.addFilter("nextRace", (races) => {
+//         console.log(races);
+//         return;
+//       })
+//     });
 
-    return await edge.handleResponse();
+//     return await edge.handleResponse();
 
 
     
-  } catch (e) {
-    console.log("ERROR", { e });
-    return context.next(e);
-  }
-};
+//   } catch (e) {
+//     console.log("ERROR", { e });
+//     return context.next(e);
+//   }
+// };
